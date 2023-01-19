5f3729ea3f6f53af8c2018346b5097bb124b9b65
franneck94 <schaffranek.jan@web.de>
2023-01-19 13:31:51 +0100

    up

--- Chapter13_VectorStruct/Main.c
+++ Chapter13_VectorStruct/Main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,16 +9,55 @@ int main()
     Vector *v1 = createVector(3, 1);
     Vector *v2 = createVector(3, -1);
 
+    assert(v1 != NULL);
+    assert(v2 != NULL);
+
+    assert(v1->data[0] == 1);
+    assert(v1->data[1] == 1);
+    assert(v1->data[2] == 1);
+    assert(v1->length == 3);
+
+    assert(v2->data[0] == -1);
+    assert(v2->data[1] == -1);
+    assert(v2->data[2] == -1);
+    assert(v2->length == 3);
+
     Vector *v3 = addVectors(v1, v2);
     printVector(v3);
+
+    assert(v3 != NULL);
+    assert(v3->data[0] == 0);
+    assert(v3->data[1] == 0);
+    assert(v3->data[2] == 0);
+    assert(v3->length == 3);
+
     Vector *v4 = subVectors(v1, v2);
     printVector(v4);
+
+    assert(v4 != NULL);
+    assert(v4->data[0] == 2);
+    assert(v4->data[1] == 2);
+    assert(v4->data[2] == 2);
+    assert(v4->length == 3);
 
     float scalar = 2.0f;
     Vector *v5 = multiplyScalar(v1, scalar);
     printVector(v5);
+
+    assert(v5 != NULL);
+    assert(v5->data[0] == 2);
+    assert(v5->data[1] == 2);
+    assert(v5->data[2] == 2);
+    assert(v5->length == 3);
+
     Vector *v6 = divideScalar(v1, scalar);
     printVector(v6);
+
+    assert(v6 != NULL);
+    assert(v6->data[0] == 0.5F);
+    assert(v6->data[1] == 0.5F);
+    assert(v6->data[2] == 0.5F);
+    assert(v6->length == 3);
 
     (void)writeOutVectorData(v6, "v6.txt");
     Vector *v6_copy = createVector(3, 0);
